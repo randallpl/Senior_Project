384b22858b89222787326058f2e5a656d930f8cf
GabeA <justclassicg530@gmail.com>
2019-11-27 20:08:16 -0800

    Added Starter to NPW transition test

--- Map_Reader/Tests/Starter_test.py
+++ Map_Reader/Tests/Starter_test.py
@@ -1,5 +1,6 @@
 from Map_Reader.Windows import StarterWindow
 from Map_Reader.ProjectController import ProjectController
+from PyQt5.QtWidgets import QWizard
 from PyQt5 import QtCore
 import pytest
 import time
@@ -25,3 +26,23 @@ def test_2(qtbot, window):
     qtbot.mouseClick(window.aboutScreen.cancelButton, QtCore.Qt.LeftButton)
 
     assert window.aboutScreen.isActiveWindow() == False
+
+def test_3(qtbot, window):
+    '''
+    Test creating a new project and going to new project wizard
+    '''
+
+    pc = ProjectController()
+
+    def on_timeout():
+
+
+        assert pc.npw.isActiveWindow() == True
+
+        qtbot.mouseClick(pc.npw.button(QWizard.CancelButton), QtCore.Qt.LeftButton)
+
+        assert pc.npw.isActiveWindow() == False
+
+
+    QtCore.QTimer.singleShot(0, on_timeout)
+    qtbot.mouseClick(pc.sw.newButton, QtCore.Qt.LeftButton)
